85da2aeaf105cba294ed794652bd7c124306c2b3
lvoepel <44590644+lvoepel@users.noreply.github.com>
2019-02-19 15:02:26 -0800

    Add files via upload
    Added visualization of results using OpenCV

--- working/planning.py
+++ working/planning.py
@@ -1,5 +1,7 @@
 import numpy as np
 from pyswip import Prolog
+from colorama import Fore, Back, Style
+import cv2
 
 #simulates steps taken while the monkey is pushing the box
 def pushing(steps):
@@ -100,24 +102,36 @@ def upRamp(steps):
     #print("went up ramp!")
     return steps
 
-def drawGrid():
+def drawGrid(gX,gY,steps, ):
     print("Setting the Scene!")
+    
 
 if __name__ == '__main__' :
     #initialize prolog
     prolog = Prolog()
     prolog.consult("monkey.pl")
-
+    
+    #gX = str(raw_input("Enter X size of graph: "))
+    #gY = str(raw_input("Enter Y size of graph: "))
+    gX = 7
+    gY = 9
     #get User input for where everything is(will be changed to opencv)
-    mX = str(raw_input("Enter Monkey X: "))
-    mY = str(raw_input("Enter Monkey Y: "))
+    mX = int(raw_input("Enter Monkey X: "))
+    mY = int(raw_input("Enter Monkey Y: "))
+    #mX = 4
+    #mY = 2
 
-    boX = str(raw_input("Enter box X: "))
-    boY = str(raw_input("Enter box Y: "))
+    boX = int(raw_input("Enter box X: "))
+    boY = int(raw_input("Enter box Y: "))
+    #boX = 3
+    #boY = 1
 
-    baX = str(raw_input("Enter banana X: "))
-    baY = str(raw_input("Enter banana Y: "))
+    baX = int(raw_input("Enter banana X: "))
+    baY = int(raw_input("Enter banana Y: "))
+    #baX = 2
+    #baY = 2
 
+    scale = 80
     #assert positions of everything
     prolog.assertz("at(monkey,["+str(mX) +"," + str(mY) +"])")
     prolog.assertz("at(box,["+str(boX) +"," + str(boY) +"])")
@@ -133,6 +147,87 @@ if __name__ == '__main__' :
     if steps:
         for step in steps:
             print(step['Xm'], step['Ym'], step["Dir"])
+    
+    img = np.ones((gX*scale+3,gY*scale+3,3), np.uint8)*255
+    x = 0
+    y = 0
+    #draw grid
+    while y < gY*scale:
+        while x < gX*scale:
+            cv2.rectangle(img,(y,x),(y+scale,x+scale),(0,0,0),3)
+            #cv2.rectangle(img,(y+10,x+10),(y+40,x+40),(0,255,0),3)
+            print("currently at " + str(x) +','+ str(y))
+            print("monkey at " + str(mX) +','+ str(mY))
+            print("result " + str(x/scale) +','+ str(y/scale))
+            if(mX == x/scale and mY == y/scale):
+                print("monkey here " + str(x) +','+ str(y))
+                cv2.rectangle(img,(x+10,y+10),(x+scale-10,y+scale-10),(19,69,139),3)
+            elif(boX == x/scale and boY == y/scale):
+                cv2.rectangle(img,(x+10,y+10),(x+scale-10,y+scale-10),(0,0,255),3)
+            elif(baX == x/scale and baY == y/scale):
+                cv2.rectangle(img,(x+10,y+10),(x+scale-10,y+scale-10),(0,255,255),3)
+            x = x + scale
+        x = 0
+        y = y + scale
+    i = 0
+    for step in steps:
+        i = i + 1
+        x = step['Xm']*scale+(scale/2)
+        y = step['Ym']*scale+(scale/2)
+        if step["Dir"] == 1:
+            cv2.line(img,(x,y),(x,y-scale),(255-i*15,0,0),5)
+        elif step["Dir"] == 2:
+            cv2.line(img,(x,y),(x-scale,y-scale),(255-i*15,0,0),5)
+        elif step["Dir"] == 3:
+            cv2.line(img,(x,y),(x-scale,y),(255-i*15,0,0),5)
+        elif step["Dir"] == 4:
+            cv2.line(img,(x,y),(x-scale,y+scale),(255-i*15,0,0),5)
+        elif step["Dir"] == 5:
+            cv2.line(img,(x,y),(x,y+scale),(255-i*15,0,0),5)
+        elif step["Dir"] == 6:
+            cv2.line(img,(x,y),(x+scale,y+scale),(255-i*15,0,0),5)
+        elif step["Dir"] == 7:
+            cv2.line(img,(x,y),(x+scale,y),(255-i*15,0,0),5)
+        elif step["Dir"] == 8:
+            cv2.line(img,(x,y),(x+scale,y-scale),(255-i*15,0,0),5)
+    '''
+    cv2.line(img,(5*50+25,5*50+25),(5*50+25,5*50+75),(255,0,0),5)
+    cv2.line(img,(5*50+25,5*50+25),(5*50+75,5*50+75),(255,0,255),5)
+    cv2.line(img,(5*50+25,5*50+25),(5*50+75,5*50+25),(0,0,255),5) 
+    '''
+    while True:
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
+    x = 0
+    y = 0
+    k = 0
+    while y < int(gY):
+        while x < int(gX):
+            if mX == x and mY == y:
+                print('[m]'),
+            elif boX == x and boY == y:
+                print('[#]'),
+            elif baX == x and baY == y:
+                print('[(]'),
+            else:   
+                print('[ ]')
+            for step in steps:
+                if step['Xm'] == x and step['Ym'] == y:
+                    print(Back.WHITE + '[ ]'+ Style.RESET_ALL),
+
+            #print(),
+            x = x + 1
+        print
+        x = 0
+        y = y + 1
+    #print(Back.White + 'and with a green background')
+    print(Style.DIM + 'and in dim text')
+    print(Style.RESET_ALL)
+    print('back to normal now')
     #drawGrid()
     #drawPlan()
 
