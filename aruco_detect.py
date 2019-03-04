7037960c97a797f14308256d81dd723268ae5846
Philip Arola <parola@pdx.edu>
2019-03-03 20:53:45 -0800

    Improved organization

--- aruco_detect.py
+++ aruco_detect.py
@@ -7,11 +7,32 @@ import cv2.aruco as aruco
 # Defines
 gridDimensions = 5
 
+class Object_Telemetry:
+    """Position data for an individual game piece
+    """
+    x = -1
+    y = -1
+    rotation = -1
+class All_Objects:
+    """Position data for all the game pieces
+    """
+    monkey = Object_Telemetry()
+    banana = Object_Telemetry()
+    ramp = Object_Telemetry()
 
-def frame_loop(cap):
-    monkey_location = [-1,-1]
-    banana_location = [-1,-1]
-    ramp_location   = [-1,-1]
+def frame_loop(cap, display=False):
+    """capture aruco
+    
+    Arguments:
+        cap {open cv capture object} -- the opencv device to view
+    
+    Keyword Arguments:
+        display {bool} -- Print debug to stdout and display the OpenCV frame (default: {False})
+    
+    Returns:
+        All_Objects -- An All_Objects object containing the detected positions of each object
+    """
+    positions = All_Objects()
 
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -31,14 +52,7 @@ def frame_loop(cap):
     # my problem was that the cellphone put black all around it. The alrogithm
     # depends very much upon finding rectangular black blobs
 
-    gray = aruco.drawDetectedMarkers(gray, corners)
 
-    #print(rejectedImgPoints)
-    # Add indicatiors and grid
-    for i in range(1, gridDimensions):
-        cv2.line(gray, ((width/gridDimensions)*i, 0), ((width/gridDimensions)*i, height), (255, 255, 0), 1, 1)
-    for i in range(1, gridDimensions):
-        cv2.line(gray, (0, (height/gridDimensions)*i), (width, (height/gridDimensions)*i), (255, 255, 0), 1, 1)
 
     for count, i in enumerate(corners):
         #x = (int(corners[i-1][0][0][0]) + int(corners[i-1][0][1][0]) + int(corners[i-1][0][2][0]) + int(corners[i-1][0][3][0])) / 4
@@ -50,32 +64,41 @@ def frame_loop(cap):
         y = (j[1] + j[3] + j[5] + j[7])/4
         if ids[count] == 1:
             print("monkey")
-            monkey_location[0]=x
-            monkey_location[1]=y
+            positions.monkey.x=x
+            positions.monkey.y=y
         elif ids[count] == 2:
             print("banana")
-            banana_location[0]=x
-            banana_location[1]=y
+            positions.banana.x=x
+            positions.banana.y=y
         elif ids[count] == 3:
             print("ramp")
-            ramp_location[0]=x
-            ramp_location[1]=y
+            positions.ramp.x=x
+            positions.ramp.y=y
         print(x, y)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+    if display is True:
+        gray = aruco.drawDetectedMarkers(gray, corners)
 
-    return monkey_location,banana_location,ramp_location
+        #print(rejectedImgPoints)
+        # Add indicatiors and grid
+        for i in range(1, gridDimensions):
+            cv2.line(gray, ((width/gridDimensions)*i, 0), ((width/gridDimensions)*i, height), (255, 255, 0), 1, 1)
+        for i in range(1, gridDimensions):
+            cv2.line(gray, (0, (height/gridDimensions)*i), (width, (height/gridDimensions)*i), (255, 255, 0), 1, 1)
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+
+    return positions
 
 
 def main():
 # Setup webcam
     cap = cv2.VideoCapture(0)
     while(True):
-        hel,llo,worl = frame_loop(cap)
-        print(hel)
-        print(llo)
-        print(worl)
+        positions = frame_loop(cap, True)
+        print(positions.monkey.x, positions.monkey.y)
+        print(positions.banana.x, positions.banana.y)
+        print(positions.ramp.x, positions.ramp.y)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
