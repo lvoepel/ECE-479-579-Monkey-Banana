a32262507a93e301dc25a8f301f5b797aea02103
Philip Arola <parola@pdx.edu>
2019-03-03 16:37:57 -0800

    Able to find coordinates for each target

--- aruco_detect.py
+++ aruco_detect.py
@@ -3,41 +3,62 @@
 import numpy as np
 import cv2
 import cv2.aruco as aruco
- 
- 
+
+# Defines
+gridDimensions = 5
+
+# Setup webcam
 cap = cv2.VideoCapture(0)
- 
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    #print(frame.shape) #480x640
+    #print(frame.shape) # Uncomment to determine dimensions
+    width = frame.shape[1]
+    height = frame.shape[0]
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters =  aruco.DetectorParameters_create()
- 
-    #print(parameters)
- 
-    '''    detectMarkers(...)
-        detectMarkers(image, dictionary[, corners[, ids[, parameters[, rejectedI
-        mgPoints]]]]) -> corners, ids, rejectedImgPoints
-        '''
-        #lists of ids and the corners beloning to each id
+
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    print(corners)
- 
+    #print(corners)
+    #print(ids)
+
     #It's working.
     # my problem was that the cellphone put black all around it. The alrogithm
     # depends very much upon finding rectangular black blobs
- 
+
     gray = aruco.drawDetectedMarkers(gray, corners)
- 
+
     #print(rejectedImgPoints)
+    # Add indicatiors and grid
+    for i in range(1, gridDimensions):
+        cv2.line(gray, ((width/gridDimensions)*i, 0), ((width/gridDimensions)*i, height), (255, 255, 0), 1, 1)
+    for i in range(1, gridDimensions):
+        cv2.line(gray, (0, (height/gridDimensions)*i), (width, (height/gridDimensions)*i), (255, 255, 0), 1, 1)
+
+    for count, i in enumerate(corners):
+        #x = (int(corners[i-1][0][0][0]) + int(corners[i-1][0][1][0]) + int(corners[i-1][0][2][0]) + int(corners[i-1][0][3][0])) / 4
+        #y = (int(corners[i-1][0][0][1]) + int(corners[i-1][0][1][1]) + int(corners[i-1][0][2][1]) + int(corners[i-1][0][3][1])) / 4
+        #print(x, y)
+        j = i.ravel()
+        print(j)
+        x = (j[0] + j[2] + j[4] + j[6])/4
+        y = (j[1] + j[3] + j[5] + j[7])/4
+        if ids[count] == 1:
+            print("monkey")
+        elif ids[count] == 2:
+            print("banana")
+        elif ids[count] == 3:
+            print("ramp")
+        print(x, y)
+
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
+
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
