76352d2d51eb53a2e4e0aa17e359a34404044c14
lvoepel <44590644+lvoepel@users.noreply.github.com>
2019-03-19 16:24:58 -0700

    Add files via upload

--- working/planning.py
+++ working/planning.py
@@ -151,7 +151,9 @@ def externalCall(positions):
     steps = []
     #call to moving which produces a list of steps based on where we can move to 
     steps = moving(steps, prolog)
-    
+    if(not steps):
+        steps = upRamp(steps, prolog)
+        steps = climbUp(steps, prolog)
     #print out list version of plan
     print("Plan:")
     if steps:
@@ -241,7 +243,9 @@ if __name__ == '__main__' :
     steps = []
     #call to moving which produces a list of steps based on where we can move to 
     steps = moving(steps, prolog)
-    
+    if(not steps):
+        steps = upRamp(steps, prolog)
+        steps = climbUp(steps, prolog)
     #print out list version of plan
     print("Plan:")
     if steps:
