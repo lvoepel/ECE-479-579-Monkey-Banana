c1347bcd9a6e95c9aa7f97f650163a6278a286f5
Rahul Marathe <36820801+RahulMarathe94@users.noreply.github.com>
2019-03-23 06:32:05 -0700

    Changed the reset positions for the servos

--- ServoMovement/ServoMovement.py
+++ ServoMovement/ServoMovement.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # File: Servo Movement.py
 # Date: 2/23/2019
 # Portland State University
@@ -8,49 +7,52 @@
 # from dynamixel_msgs.msg import JointState
 # from std_msgs.msg import Float64
 # from std_msgs.msg import String
-
-
 import os
 import time
-
+import keyboard
 import serial
 from pyax12.connection import Connection
 
 
-
+"""
 pub1  = rospy.Publisher('/6/command', Float64)
 pub2  = rospy.Publisher('/7/command', Float64)
 pub3  = rospy.Publisher('/8/command', Float64)
 pub4  = rospy.Publisher('/9/command', Float64)
 pub5  = rospy.Publisher('/10/command', Float64)
-
+"""
 
 POSSIBLE_POSITIONS = ["Reset", "Pickup", "Collect"]
-
-
+a=0
+b=0
+c=0
+d=0
+e=0
+flag=0
 SERVO_SPEED = 75
 # use the dictionary to map the positions. 2/3 Possible combinations for the arm to move
 SERVO_SETTING = {"Collect":
                        {"a": 0,
-                        "b": 0,
+                        "b":0,
                         "c": 0,
                         "d": -90,
                         "e": 60},
                  "Pickup":
-                     {"a": 0,
-                      "b": 50,
-                      "c": 20,
-                      "d": -50,
-                      "e": 90,
+                     {"a": 150,
+                      "b": 0,
+                      "c": 0,
+                      "d": -90,
+                      "e": 60,
                       },
-                 "Reset":{"a": 0,
-                        "b": 0,
-                        "c": 0,
-                        "d": -90,
+                 "Reset":{"a": 75,
+                        "b": -50,
+                        "c": -100,
+                        "d": -15,
                         "e": 60
                             }
 
                       }
+        #A: 75 B: -50 C: -100 D: -15 E: 60
 
 class ServoMovement:
     def __init__(self, input_servo_connection):
@@ -59,6 +61,7 @@ class ServoMovement:
     def pick_up(self):
         # time.sleep(1.5)
         print("Pick Up")
+        global a,b,c,d,e
 
         """
         pub1.publish(float(SERVO_SETTING[POSSIBLE_POSITIONS[1]]["a"]))
@@ -68,33 +71,95 @@ class ServoMovement:
         pub5.publish(float(SERVO_SETTING[POSSIBLE_POSITIONS[1]]["e"]))
         # delay between motions
         rospy.sleep(float(delay))
+        
         """
+        b=-25
+        e=60
+        self.input_object.goto(7,b, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(10, e, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
 
 
         self.input_object.goto(6, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["a"], speed=SERVO_SPEED, degrees=True)
-        time.sleep(0.5)
-        self.input_object.goto(7, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["b"], speed=SERVO_SPEED, degrees=True)
         time.sleep(0.5)
         self.input_object.goto(8, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["c"], speed=SERVO_SPEED, degrees=True)
         time.sleep(0.5)
         self.input_object.goto(9, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["d"], speed=SERVO_SPEED, degrees=True)
         time.sleep(2.0)
+        b=0
+        self.input_object.goto(7, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["b"], speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
         e = 100
-        self.input_object.goto(10, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["e"], speed=SERVO_SPEED, degrees=True)
+        self.input_object.goto(10, e, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        b=-25
+        self.input_object.goto(7, b, speed=SERVO_SPEED, degrees=True)
         time.sleep(0.5)
         print("A: {} B: {} C: {} D: {} E: {}".format(SERVO_SETTING[POSSIBLE_POSITIONS[1]]["a"],
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[1]]["b"],
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[1]]["c"],
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[1]]["d"],
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[1]]["e"]))
+
+
+        """
+        self.input_object.goto(6, a, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(7, b, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(8, c, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(9, d, speed=SERVO_SPEED, degrees=True)
+        time.sleep(2.0)
+        #e = 100
+        self.input_object.goto(10, e, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        print("A: {} B: {} C: {} D: {} E: {}".format(a,
+                                                     b,
+                                                     c,
+                                                     d,
+                                                    e))
+
+        """
+
         return
+
+    def place(self):
+        print("ENtered Place")
+        b = -25
+        e = 100
+        self.input_object.goto(7, b, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(10, e, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(6, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["a"], speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+
+        self.input_object.goto(8, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["c"], speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        self.input_object.goto(9, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["d"], speed=SERVO_SPEED, degrees=True)
+        time.sleep(2.0)
+        b = 0
+        self.input_object.goto(7, SERVO_SETTING[POSSIBLE_POSITIONS[1]]["b"], speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        e = 60
+        self.input_object.goto(10, e, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        b = -25
+        self.input_object.goto(7, b, speed=SERVO_SPEED, degrees=True)
+        time.sleep(0.5)
+        print("A: {} B: {} C: {} D: {} E: {}".format(SERVO_SETTING[POSSIBLE_POSITIONS[1]]["a"],
+                                                     b,
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[1]]["c"],
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[1]]["d"],
+                                                     e))
 
 
 
     def collect(self):
         print("Return")
         # time.sleep(1.5)
-
         """
              pub1.publish(float(SERVO_SETTING[POSSIBLE_POSITIONS[2]]["a"]))
              pub2.publish(float(SERVO_SETTING[POSSIBLE_POSITIONS[2]]["b"]))
@@ -126,6 +191,14 @@ class ServoMovement:
 
     def reset(self):
         print("Reset Positions")
+        global a,b,c,d,e
+
+        print("A: {} B: {} C: {} D: {} E: {}".format(SERVO_SETTING[POSSIBLE_POSITIONS[0]]["a"],
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[0]]["b"],
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[0]]["c"],
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[0]]["d"],
+                                                     SERVO_SETTING[POSSIBLE_POSITIONS[0]]["e"]))
+
 
         """
                  pub1.publish(float(SERVO_SETTING[POSSIBLE_POSITIONS[0]]["a"]))
@@ -141,6 +214,7 @@ class ServoMovement:
         time.sleep(0.5)
         self.input_object.goto(7, SERVO_SETTING[POSSIBLE_POSITIONS[0]]["b"], speed=SERVO_SPEED, degrees=True)
         time.sleep(0.5)
+        print("C: {}".format(c))
         self.input_object.goto(8, SERVO_SETTING[POSSIBLE_POSITIONS[0]]["c"], speed=SERVO_SPEED, degrees=True)
         time.sleep(0.5)
         self.input_object.goto(9, SERVO_SETTING[POSSIBLE_POSITIONS[0]]["d"], speed=SERVO_SPEED, degrees=True)
@@ -153,11 +227,66 @@ class ServoMovement:
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[0]]["d"],
                                                      SERVO_SETTING[POSSIBLE_POSITIONS[0]]["e"]))
 
+        a=0
+        b=0
+        c=0
+        d=-90
+        e=60
+
+
+
+
         return
+
+
+def get_values():
+    #print("In get Values")
+    global a,b,c,d,e,flag
+    if(keyboard.is_pressed("q")):
+       a=a+25
+       flag = 1
+    elif (keyboard.is_pressed("a")):
+        a=a-25
+        flag = 1
+    elif (keyboard.is_pressed("w")):
+        b = b + 25
+        flag = 1
+    elif (keyboard.is_pressed("s")):
+        b = b - 25
+        flag = 1
+    elif (keyboard.is_pressed("e")):
+        c = c + 25
+        flag = 1
+    elif (keyboard.is_pressed("d")):
+        c = c - 25
+        flag = 1
+
+    elif (keyboard.is_pressed("r")):
+        d = d + 25
+        flag = 1
+    elif (keyboard.is_pressed("f")):
+        d = d - 25
+        flag = 1
+
+    elif (keyboard.is_pressed("t")):
+        e = e + 25
+        flag = 1
+    elif (keyboard.is_pressed("g")):
+        e = e - 25
+        flag = 1
+
+    elif(keyboard.is_pressed("m")):
+        servo_class.reset()
+
+
+
+    return
+
 
 def motion_play():
     servo_class.pick_up()
     servo_class.collect()
+    servo_class.place()
     return
 
 def rospy():
@@ -173,11 +302,23 @@ if __name__ == "__main__":
     servo_class = ServoMovement(serial_connection)
     # reset the hand before1
     servo_class.reset()
+    motion_play()
+    servo_class.reset()
+
+    """
+    while(1):
+        get_values()
+        if(flag==1):
+            print("FLag Set")
+            flag=0
+            motion_play()
+
+
     try:
         rospy()
     except rospy.ROSInterruptException:
         pass
-
+    """
 
 
  # Reset Position claw should be closed.
